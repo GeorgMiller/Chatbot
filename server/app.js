69f50558bcadf384608150434c6ea1d9e9dec9bd
wolf <wolf-rieder@t-online.de>
2021-06-10 20:47:54 +0200

    audio interface

--- server/app.js
+++ server/app.js
@@ -1,24 +1,32 @@
 const express = require("express"),
   app = express(),
-  path = require("path");
-(http = require("http").Server(app)),
-  (socketio = require("socket.io")(http, {
+  path = require("path"),
+  fs = require("fs"),
+  ms = require("mediaserver"),
+  http = require("http").Server(app),
+  socketio = require("socket.io")(http, {
     cors: {
       origin: "http://localhost:8080",
       methods: ["GET", "POST"],
       credentials: true,
     },
-  }));
+  });
+
+const ss = require("socket.io-stream");
 
 let position = {
   x: 100,
   y: 100,
 };
 
-app.use("/", express.static(path.join(__dirname, "public")));
+// app.use("/", express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/login.html"));
+});
+
+app.get("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index_old.html"));
 });
 
 socketio.on("connection", (socket) => {
@@ -48,6 +56,23 @@ socketio.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
+  });
+
+  socket.on("message", async function (data) {
+    console.log(data);
+
+    const dataURL = data.audio.dataURL.split(",").pop();
+    let fileBuffer = Buffer.from(dataURL, "base64");
+    console.log(fileBuffer);
+
+    fs.writeFileSync("test.wav", fileBuffer, function (err) {
+      res.sendStatus(err ? 500 : 200);
+    });
+
+    // ms.pipe("", "", path.join(__dirname, "/login.html"));
+
+    const results = "HHHH";
+    socket.emit("results", results);
   });
 });
 
